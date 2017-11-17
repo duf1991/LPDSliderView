267b5c908d83c09cf77a611a5b2f38b84165f1ae
Du Yingfeng <yingfeng.du02@ele.me>
2017-11-17 11:58:07 +0800

    ccc

--- Example/LPDSliderView/LPDViewController.m
+++ Example/LPDSliderView/LPDViewController.m
@@ -18,11 +18,13 @@
 {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    
+    //初始化frame以及滑块宽度，滑块图片
     _sliderView = [[LPDSliderView alloc] initWithFrame:CGRectMake(40, 100, 300, 80) withHandleWith:80 withHandleImage:[UIImage imageNamed:@"icon_arrow_slide"]];
 //    [_sliderView.layer setBorderWidth:0.0];//可以设置是否显示边框
+    //设置背景颜色，滑块颜色，滑块左部view颜色，整个边框颜色
     [_sliderView setColorsForBackground:[UIColor grayColor] foreground:[UIColor blueColor] handle:[UIColor redColor] border:nil];
     _sliderView.delegate = self;
+    //设置文本的相关属性
     _sliderView.label.text = @"确认到店";
     _sliderView.label.font = [UIFont systemFontOfSize:25];
     _sliderView.label.textColor = [UIColor whiteColor];
@@ -30,11 +32,12 @@
     
 
 }
-
+#pragma mark -- LPDSliderView代理方法
+//随时监听滑块的滑动位置
 - (void)sliderValueChanged:(LPDSliderView *)sender {
-    
+    NSLog(@"滑块位置：%f",sender.value);
 }
-
+//监听滑动结束时滑块的位置
 - (void)sliderValueChangeEnded:(LPDSliderView *)sender {
     if(_sliderView.value == 1){
         NSLog(@"欧耶！");
